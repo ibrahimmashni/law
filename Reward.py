9f9d746e3e6da34dae3ea611a58562a8ea8da65d
IBRAHIM A MASHNI <132998501+ibrahimmashni@users.noreply.github.com>
2023-05-09 20:30:39 +0300

    Create Reward.py

---
+++ Reward.py
@@ -0,0 +1,4 @@
+salary = float(input("Enter the employee's base salary: "))
+years_of_service = int(input("Enter the employee's number of years of service: "))
+bonus = salary * 12 * 0.14 * years_of_service
+print("The one-time reward amount is: ", bonus)
